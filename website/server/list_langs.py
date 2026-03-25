b3461b918707fecd823223b4683fdafab349b520
Mohamed Mosad <mohamedmosad1223@gmail.com>
2026-03-25 22:10:02 +0200

    delet website files

--- website/server/list_langs.py
+++
@@ -1,8 +0,0 @@
-from app.database import SessionLocal
-from app.models.reference import Language
-
-db = SessionLocal()
-langs = db.query(Language).all()
-for l in langs:
-    print(f"ID: {l.language_id}, Code: {l.language_code}, Name: {l.language_name}")
-db.close()
