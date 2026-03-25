db8dd15e6027ffd5c96df75d8ecb27e00caacf65
Mohamed Mosad <mohamedmosad1223@gmail.com>
2026-03-25 22:10:02 +0200

    delet website files

--- website/server/app/controllers/help_controller.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-from app.models.faq import FAQ
-
-class HelpCenterController:
-
-    @staticmethod
-    def list_faqs(db: Session):
-        return db.query(FAQ).filter(FAQ.is_active == True).all()
